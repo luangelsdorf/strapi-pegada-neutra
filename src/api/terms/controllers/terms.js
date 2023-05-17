f871e953cfed4a82d6e196bc4b0a5b6df45ac881
luangelsdorf <luanferreira2136@gmail.com>
2023-05-17 10:16:11 -0300

    Content updates

---
+++ src/api/terms/controllers/terms.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * terms controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::terms.terms');
---
+++ src/api/terms/routes/terms.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * terms router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::terms.terms');
